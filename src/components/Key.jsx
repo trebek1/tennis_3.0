c5967d32953346a1694ab8cab18746f9ed45d569
Alex Trzeciak <trebek08@gmail.com>
2017-07-08 17:45:24 -0700

    added pin images and added ids for key targets

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -9,10 +9,10 @@ export default class CourtList extends Component {
     	return(
     		<div>
     			<span id="key"> Map Key</span>
-				<div className="ib"><span className="keyValue"><img src={url + "F8EC3B"} /></span><span className="keyValue leftSpace">Tennis Club</span></div>
-				<div className="ib"><span className="keyValue"><img src={url + "3BF83E"} /></span><span className="keyValue leftSpace"> Public Tennis Court</span></div>
-				<div className="ib"><span className="keyValue"><img src={url + "FE7569"}/></span><span className="keyValue leftSpace"> Tennis Shop</span></div>
-				<div className="ib"><span className="keyValue"><img src={url + "00ccff"} /></span><span className="keyValue leftSpace"> Other Facility</span></div>	
+				<div id="keyClub"     className="ib club"><span className="keyValue"><img src={url + "F8EC3B"} /></span><span className="keyValue leftSpace">Tennis Club</span></div>
+				<div id="keyCourt"    className="ib court"><span className="keyValue"><img src={url + "3BF83E"} /></span><span className="keyValue leftSpace"> Public Tennis Court</span></div>
+				<div id="keyShop"     className="ib shop"><span className="keyValue"><img src={url + "FE7569"}/></span><span className="keyValue leftSpace"> Tennis Shop</span></div>
+				<div id="keyFacility" className="ib facility"><span className="keyValue"><img src={url + "00ccff"} /></span><span className="keyValue leftSpace"> Other Facility</span></div>	
     		</div>
     	) 	
   	}
