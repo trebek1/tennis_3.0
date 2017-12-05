ebb1e8d8c00724c45d725849b5cf44dc5b94df07
Alex Trzeciak <trebek08@gmail.com>
2017-12-04 17:52:59 -0800

    linting for project

--- src/actions/courtActions.js
+++ src/actions/courtActions.js
@@ -1,34 +1,29 @@
-"use strict"
+import axios from 'axios';
 
-import axios from "axios"; 
+export function getCourts() {
+  return (dispatch) => {
+    axios.get('/courts')
+      .then((response) => {
+        dispatch({ type: 'GET_COURTS', payload: response.data });
+      })
+      .catch(() => {
+        // console.log('an error occurred ', err);
+      });
+  };
+}
 
-export function getCourts(){
-	return function(dispatch){
-		axios.get("/courts")
-		.then(function(response){
-			dispatch({type: "GET_COURTS", payload: response.data});
-		})
-		.catch(function(err){
-			// dispatch({type: "GET_BOOKS_REJECTED", payload: err});
-			console.log("an error occurred"); 
-		});	
-	};
-};
+export function selectStyle(type) {
+  return { type: 'SELECT_STYLE', payload: type };
+}
 
-export function selectStyle(type){
-	return {type: "SELECT_STYLE", payload: type};	
-};
+export function sortPoints(type) {
+  return { type: 'SORT_POINTS', payload: type };
+}
 
-export function sortPoints(type){
-	return {type: "SORT_POINTS", payload: type};
-};
+export function selectPoint(index) {
+  return { type: 'SELECT_POINT', payload: index };
+}
 
-export function selectPoint(index){
-	return {type: "SELECT_POINT", payload: index};
-};
-
-export function updateSort(sort){
-	return {type: "UPDATE_SORT", payload: sort}; 
-};
-
-	
+export function updateSort(sort) {
+  return { type: 'UPDATE_SORT', payload: sort };
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
+import { routerReducer } from 'react-router-redux';
 import { combineReducers } from 'redux';
-import login from "./login";
-import courts from "./courts";
-import styles from "./styles";
-import { routerReducer } from 'react-router-redux';	
+import login from './login';
+import courts from './courts';
+import styles from './styles';
 
 const reducers = combineReducers({
-	login,
-	courts,
-	styles,
-	routing: routerReducer
+  login,
+  courts,
+  styles,
+  routing: routerReducer,
 });
 
 export default reducers;
