de2865ed19ca12d1233cf16ff556d32aad299aa1
Alex Trzeciak <trebek08@gmail.com>
2017-12-04 20:20:19 -0800

    linting except for main map file and alias

--- src/index.jsx
+++ src/index.jsx
@@ -1,46 +1,28 @@
-//react
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, IndexRoute, Route, browserHistory} from 'react-router';
-
-
-//Redux 
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import reducers from './reducers';
-import { syncHistoryWithStore } from 'react-router-redux';	
 import thunk from 'redux-thunk';
-
-
-//components
 import AppContainer from 'AppContainer';
-// import LoginContainer from './containers/LoginContainer';
-// import SignUpContainer from './containers/SignUpContainer';
 import Wrapper from './components/Wrapper';
+import reducers from './reducers';
 
 const store = createStore(
-	reducers,
-	applyMiddleware(thunk)
-	);
-
-	// compose(
-	// 	applyMiddleware(thunk),
-	// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	//	);
-
-	// <Route path="/login" pageId="Login" component={LoginContainer} />
-	// <Route path="/signup" pageId="SignUp" component={SignUpContainer} />
+  reducers,
+  applyMiddleware(thunk),
+);
 
 const history = syncHistoryWithStore(browserHistory, store);
-
 const routes = (
-		<Provider store={store}>
-			<Router history={history}>
-				<Route path="/" pageId="wrapper" component={Wrapper}>
-					<IndexRoute pageId="index" component={AppContainer} />				
-				</Route>
-			</Router>
-		</Provider>
-	)
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" pageId="wrapper" component={Wrapper}>
+        <IndexRoute pageId="index" component={AppContainer} />
+      </Route>
+    </Router>
+  </Provider>
+);
 
 ReactDOM.render(routes, document.getElementById('root'));
