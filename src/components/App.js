418af59c26ea6fd4231688091707fe2c63d48568
Alex Trzeciak <trebek08@gmail.com>
2019-03-11 01:42:15 -0700

    remove wrapper and move router into component

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  IndexRoute
+} from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import AppContainer from "../containers/AppContainer";
+import Footer from "./Footer";
+import reducers from "../reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Switch>
+          <div className="app-wrapper">
+            <Route exact path="/" component={AppContainer} />
+            <Footer />
+          </div>
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { Provider } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  IndexRoute
-} from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import App from "./components/App";
 
-import AppContainer from "./containers/AppContainer";
-import Footer from "./components/Footer";
-import reducers from "./reducers";
-
-const store = createStore(reducers, applyMiddleware(thunk));
-const routes = (
-  <Provider store={store}>
-    <Router>
-      <div>
-        <Switch>
-          <div className="app-wrapper">
-            <Route exact path="/" component={AppContainer} />
-            <Footer />
-          </div>
-        </Switch>
-      </div>
-    </Router>
-  </Provider>
-);
-
-render(routes, document.getElementById("app"));
+render(<App />, document.getElementById("app"));
 module.hot.accept();
